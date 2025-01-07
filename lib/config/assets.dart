0196f9bcdebbdc9d81ddcccb3cbe893dc9451540
Isa-30a <isabella.alvasol@gmail.com>
2025-01-07 17:35:20 -0500

    🎉 Add initial project structure and configuration files for Flutter application

---
+++ lib/config/assets.dart
@@ -0,0 +1,11 @@
+abstract final class Assets {
+  static const bgDesktopDark = 'assets/image/bg-desktop-dark.jpg';
+  static const bgDesktopLight = 'assets/image/bg-desktop-light.jpg';
+  static const bgMobileDark = 'assets/image/bg-mobile-dark.jpg';
+  static const bgMobileLight = 'assets/image/bg-mobile-light.jpg';
+  static const iconcheck = 'assets/image/icon-check.svg';
+  static const iconcross = 'assets/image/icon-cross.svg';
+  static const iconmoon = 'assets/image/icon-moon.svg';
+  static const iconsun = 'assets/image/icon-sun.svg';
+
+}
