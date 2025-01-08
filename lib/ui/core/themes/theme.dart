cecea5c56b99fef02137e15aba10a3f742ba20b8
Isa-30a <isabella.alvasol@gmail.com>
2025-01-07 19:13:52 -0500

    🏗️ Update theme configuration and add Google Fonts support

--- lib/ui/core/themes/theme.dart
+++ lib/ui/core/themes/theme.dart
@@ -1,7 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 final lightTheme = ThemeData(
-    useMaterial3: true,
-    colorScheme: ColorScheme.fromSeed(
-      seedColor: Color.fromARGB(255, 58, 123, 253),
-    ));
+  brightness: Brightness.light,
+  primaryColor: const Color(0xFFEFEFEF), // Very Light Gray
+  scaffoldBackgroundColor: const Color(0xFFEFEFEF),
+
+  colorScheme: ColorScheme.light(
+    primary: const Color(0xFFEFEFEF), // Very Light Gray
+    secondary: const Color(0xFFCAD3E0), // Very Light Grayish Blue
+    surface: const Color(0xFFDADADA), // Light Grayish Blue
+    background: const Color(0xFFEFEFEF), // Background color
+    onPrimary: const Color(0xFF4C566A), // Dark Grayish Blue
+    onSecondary: const Color(0xFF37474F), // Very Dark Grayish Blue
+  ),
+  textTheme: GoogleFonts.josefinSansTextTheme(),
+);
+
