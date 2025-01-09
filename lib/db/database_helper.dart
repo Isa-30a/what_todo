ebdf720a8d93d2ed33001cdebc4c90c9341ec3ad
Isa-30a <isabella.alvasol@gmail.com>
2025-01-08 20:07:26 -0500

    ♻️ refactor: remove Drift dependency and migrate to SQLite for database management

---
+++ lib/db/database_helper.dart
@@ -0,0 +1,39 @@
+import 'dart:async';
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
+
+class DatabaseHelper {
+  static final DatabaseHelper _instance = DatabaseHelper._internal();
+  factory DatabaseHelper() => _instance;
+  DatabaseHelper._internal();
+
+  static Database? _database;
+
+  Future<Database> get database async {
+    if (_database != null) return _database!;
+    _database = await _initDatabase();
+    return _database!;
+  }
+
+  Future<Database> _initDatabase() async {
+    final dbPath = await getDatabasesPath();
+
+    final path = join(dbPath, 'whatTodoDb.db');
+
+    return openDatabase(
+      path,
+      version: 1,
+      onCreate: _onCreate,
+    );
+  }
+
+  Future<void> _onCreate(Database db, int version)async{
+    await db.execute('''
+      CREATE TABLE tasks (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        title TEXT NOT NULL,
+        conplete TEXT
+      )
+    ''');
+  }
+}
