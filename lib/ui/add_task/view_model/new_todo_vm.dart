edc89fe1c52b56aefa74149bb8824c220acf07a2
Isa-30a <isabella.alvasol@gmail.com>
2025-01-08 22:25:33 -0500

    🚧  WIP: implementing NewTodoVm with TaskRepository dependency injection

--- lib/ui/add_task/view_model/new_todo_vm.dart
+++ lib/ui/add_task/view_model/new_todo_vm.dart
@@ -1,5 +1,9 @@
-// class NewTodoVm {
-//   const NewTodoVm({
-  
-//   })
-// };
+import 'package:what_todo/data/repositories/task_repository.dart';
+
+class NewTodoVm {
+  const NewTodoVm({
+   required TaskRepository taskRepository,
+   }): _taskRepository = taskRepository;
+
+    final TaskRepository _taskRepository;
+};
