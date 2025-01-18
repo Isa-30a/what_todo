6b35da23a8dbc44c21ec678113fed5dd99f7331a
Isa-30a <isabella.alvasol@gmail.com>
2025-01-18 17:15:59 -0500

    fix: update asset paths to use 'assets/images/' directory

--- lib/config/assets.dart
+++ lib/config/assets.dart
@@ -1,11 +1,10 @@
 abstract final class Assets {
-  static const bgDesktopDark = 'assets/image/bg-desktop-dark.jpg';
-  static const bgDesktopLight = 'assets/image/bg-desktop-light.jpg';
-  static const bgMobileDark = 'assets/image/bg-mobile-dark.jpg';
-  static const bgMobileLight = 'assets/image/bg-mobile-light.jpg';
-  static const iconcheck = 'assets/image/icon-check.svg';
-  static const iconcross = 'assets/image/icon-cross.svg';
-  static const iconmoon = 'assets/image/icon-moon.svg';
-  static const iconsun = 'assets/image/icon-sun.svg';
-
+  static const bgDesktopDark = 'assets/images/bg-desktop-dark.jpg';
+  static const bgDesktopLight = 'assets/images/bg-desktop-light.jpg';
+  static const bgMobileDark = 'assets/images/bg-mobile-dark.jpg';
+  static const bgMobileLight = 'assets/images/bg-mobile-light.jpg';
+  static const iconcheck = 'assets/images/icon-check.svg';
+  static const iconcross = 'assets/images/icon-cross.svg';
+  static const iconmoon = 'assets/images/icon-moon.svg';
+  static const iconsun = 'assets/images/icon-sun.svg';
 }
