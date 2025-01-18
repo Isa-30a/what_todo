47c2a699cc0b2548b6655f4f0140574d83cd0400
Isa-30a <isabella.alvasol@gmail.com>
2025-01-18 17:18:56 -0500

    🚧 WIP: enhance AddTodoScreen with TasksListView and new layout elements

--- lib/ui/add_task/widgets/new_todo_screen.dart
+++ lib/ui/add_task/widgets/new_todo_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:what_todo/config/settings/settings_view.dart';
+import 'package:what_todo/src/sample_feature/tasks_list_view.dart';
 
 class AddTodoScreen extends StatelessWidget {
   const AddTodoScreen({super.key});
@@ -23,16 +24,40 @@ class AddTodoScreen extends StatelessWidget {
         ],
       ),
       body: Center(
-        child: Column(
-          children: [
-            // widget Crear nuevo task
-            
-            //lista de widgets
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              // widget Crear nuevo task
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 12),
+                child: ListTile(
+                  leading: IconButton.outlined(
+                      onPressed: () {}, icon: Icon(Icons.abc)),
+                  title: Text('Create a new todo...'),
+                ),
+              ),
 
-            //filterWidgets
-
-            //coment Drag and drop
-          ],
+              //lista de widgets
+              Container(
+                color: Colors.red,
+                height: MediaQuery.sizeOf(context).height / 3,
+                child: TasksListView(),
+              )
+              //filterWidgets
+              ,
+              //coment Drag and drop
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  TextButton(onPressed: () {}, child: Text('All')),
+                  TextButton(onPressed: () {}, child: Text('Active')),
+                  TextButton(onPressed: () {}, child: Text('Completed'))
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );
