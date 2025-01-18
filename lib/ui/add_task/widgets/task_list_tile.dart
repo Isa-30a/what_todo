9ca641b04ca3784dcb54bf541acb51b28c6a91c6
Isa-30a <isabella.alvasol@gmail.com>
2025-01-18 17:19:05 -0500

    ✨ feat: add TaskListTile widget for displaying individual tasks in the task list

---
+++ lib/ui/add_task/widgets/task_list_tile.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:what_todo/data/model/task.dart';
+
+class TaskListTile extends StatelessWidget {
+  const TaskListTile({
+    super.key,
+    required this.task,
+  });
+
+  final Task task;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      title: Text(task.title),
+      leading: CircleAvatar(
+        radius: 50,
+        // backgroundImage: AssetImage(Assets.bgMobileDark),
+        child: IconButton(
+          onPressed: () {},
+          icon: (task.completed ?? false)
+              ? Icon(Icons.check)
+              : Icon(Icons.circle_outlined),
+        ),
+      ),
+      trailing: IconButton(onPressed: () {}, icon: Icon(Icons.close)),
+    );
+  }
+}
