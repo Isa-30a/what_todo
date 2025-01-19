46c6029444aa3665ac9946bcba24f6f1152468f8
Isa-30a <isabella.alvasol@gmail.com>
2025-01-19 18:15:31 -0500

    ✨ feat: implement CRUD operations in NewTodoVm for task management

--- lib/ui/add_task/view_model/new_todo_vm.dart
+++ lib/ui/add_task/view_model/new_todo_vm.dart
@@ -1,9 +1,25 @@
+import 'package:what_todo/data/model/task.dart';
 import 'package:what_todo/data/repositories/task_repository.dart';
 
 class NewTodoVm {
   const NewTodoVm({
-   required TaskRepository taskRepository,
-   }): _taskRepository = taskRepository;
+    required TaskRepository taskRepository,
+  }) : _taskRepository = taskRepository;
 
-    final TaskRepository _taskRepository;
+  final TaskRepository _taskRepository;
+  Future<List<Task>> getAllTasks() async {
+    return await _taskRepository.getAllTasks();
+  }
+
+  Future<int> insertTask(Task task) async {
+    return await _taskRepository.insertTask(task);
+  }
+
+  Future<int> deleteTask(int id) async {
+    return await _taskRepository.deleteTask(id);
+  }
+
+  Future<bool> updateTask(Task task) async {
+    return await _taskRepository.updateTask(task);
+  }
 }
