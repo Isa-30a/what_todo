1b2297dff977569ff2c7e5f71807c750ee70a7f9
Isa-30a <isabella.alvasol@gmail.com>
2025-01-19 17:23:23 -0500

    ✨ feat: enhance AddTodoScreen layout with background image and improved input card

--- lib/ui/add_task/widgets/new_todo_screen.dart
+++ lib/ui/add_task/widgets/new_todo_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:what_todo/config/assets.dart';
 import 'package:what_todo/config/settings/settings_view.dart';
 import 'package:what_todo/src/sample_feature/tasks_list_view.dart';
 
@@ -8,9 +9,12 @@ class AddTodoScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final controller = TextEditingController();
+
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Sample Items'),
+        backgroundColor: Colors.transparent,
+        title: const Text('TODO'),
         actions: [
           IconButton(
             icon: const Icon(Icons.settings),
@@ -23,42 +27,67 @@ class AddTodoScreen extends StatelessWidget {
           ),
         ],
       ),
-      body: Center(
-        child: SingleChildScrollView(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              // widget Crear nuevo task
-              Padding(
-                padding: EdgeInsets.symmetric(horizontal: 12),
-                child: ListTile(
-                  leading: IconButton.outlined(
-                      onPressed: () {}, icon: Icon(Icons.abc)),
-                  title: Text('Create a new todo...'),
-                ),
+      body: Stack(
+        children: [
+          Container(
+            height: MediaQuery.sizeOf(context).height / 4,
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage(Assets.bgDesktopDark),
+                fit: BoxFit.cover,
               ),
-
-              //lista de widgets
-              Container(
-                color: Colors.red,
-                height: MediaQuery.sizeOf(context).height / 3,
-                child: TasksListView(),
-              )
-              //filterWidgets
-              ,
-              //coment Drag and drop
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  TextButton(onPressed: () {}, child: Text('All')),
-                  TextButton(onPressed: () {}, child: Text('Active')),
-                  TextButton(onPressed: () {}, child: Text('Completed'))
-                ],
-              )
-            ],
+            ),
           ),
-        ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                // widget Crear nuevo task
+                Card(
+                  child: ListTile(
+                    leading: TextButton(
+                      onPressed: () {},
+                      child: Icon(Icons.circle_outlined),
+                    ),
+                    title: TextField(
+                      decoration: InputDecoration(
+                        border: InputBorder.none,
+                        hintText: 'Create a new todo...',
+                      ),
+                      textAlign: TextAlign.left,
+                      maxLines: 1,
+                      controller: controller,
+                      onSubmitted: (String value) {},
+                    ),
+                  ),
+                ),
+
+                //lista de widgets
+
+                SizedBox(
+                  height: MediaQuery.sizeOf(context).height / 3,
+                  child: TasksListView(),
+                ),
+                //filterWidgets
+
+                //coment Drag and drop
+
+                Card(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      TextButton(onPressed: () {}, child: Text('All')),
+                      TextButton(onPressed: () {}, child: Text('Active')),
+                      TextButton(onPressed: () {}, child: Text('Completed'))
+                    ],
+                  ),
+                ),
+                Text('Drag and drop to reorder list')
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
