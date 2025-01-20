3e1a9fb548e0bbb9ebb86b0aa925cc626292c71f
Isa-30a <isabella.alvasol@gmail.com>
2025-01-20 18:42:56 -0500

    ✨ feat: replace task title with editable TextField for improved user interaction in TaskListTile

--- lib/ui/add_task/widgets/task_list_tile.dart
+++ lib/ui/add_task/widgets/task_list_tile.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:what_todo/config/assets.dart';
 import 'package:what_todo/data/model/task.dart';
 import 'package:what_todo/ui/add_task/view_model/todo_provider.dart';
 
@@ -13,11 +14,27 @@ class TaskListTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final controller = TextEditingController();
+
     return ListTile(
-      title: Text(task.title),
+      title: TextField(
+        decoration: InputDecoration(
+          border: InputBorder.none,
+          hintText: 'Create a new todo...',
+        ),
+        textAlign: TextAlign.left,
+        maxLines: 1,
+        controller: controller..text = task.title,
+        onSubmitted: (String value) {
+          if (task.id != null) {
+            // Provider.of<TodoProvider>(context, listen: false)
+            //     .updateTask(task.id!, value);
+          }
+        },
+      ),
       leading: CircleAvatar(
         radius: 20,
-        // backgroundImage: AssetImage(Assets.bgMobileDark),
+        backgroundImage: AssetImage(Assets.bgMobileDark),
         child: IconButton(
           onPressed: () {},
           icon: (task.completed)
