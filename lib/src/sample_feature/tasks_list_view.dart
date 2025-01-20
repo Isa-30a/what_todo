d3c58484a0fde9efb1740053325eccc360d75bf2
Isa-30a <isabella.alvasol@gmail.com>
2025-01-19 19:50:17 -0500

    ✨ feat: refactor TasksListView to use ViewModel for task retrieval and implement FutureBuilder for async data loading

--- lib/src/sample_feature/tasks_list_view.dart
+++ lib/src/sample_feature/tasks_list_view.dart
@@ -1,21 +1,19 @@
 import 'package:flutter/material.dart';
-import 'package:what_todo/data/model/task.dart';
+import 'package:what_todo/data/repositories/task_repository.dart';
+import 'package:what_todo/data/services/task_service.dart';
+import 'package:what_todo/ui/add_task/view_model/new_todo_vm.dart';
 import 'package:what_todo/ui/add_task/widgets/task_list_tile.dart';
 
 /// Displays a list of SampleItems.
 class TasksListView extends StatelessWidget {
-  const TasksListView({
-    super.key,
-    this.tasks = const [
-      Task(title: 'NewTask', id: 0),
-      Task(id: 1, title: 'Taask')
-    ],
-  });
-
-  final List<Task> tasks;
+  const TasksListView({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final vm = NewTodoVm(
+      taskRepository: TaskRepository(taskService: TaskService()),
+    );
+    final tasks = vm.getAllTasks();
     return Scaffold(
       // To work with lists that may contain a large number of items, it’s best
       // to use the ListView.builder constructor.
@@ -24,19 +22,24 @@ class TasksListView extends StatelessWidget {
       // building all Widgets up front, the ListView.builder constructor lazily
       // builds Widgets as they’re scrolled into view.
       body: Card(
-        child: ListView.separated(
-          separatorBuilder: (BuildContext context, int index) =>
-              const Divider(),
+        child: FutureBuilder(
+          future: tasks,
+          builder: (context, snapshot) {
+            return ListView.separated(
+              separatorBuilder: (BuildContext context, int index) =>
+                  const Divider(),
 
-          // Providing a restorationId allows the ListView to restore the
-          // scroll position when a user leaves and returns to the app after it
-          // has been killed while running in the background.
-          restorationId: 'tasksListView',
-          itemCount: tasks.length,
-          itemBuilder: (BuildContext context, int index) {
-            final item = tasks[index];
+              // Providing a restorationId allows the ListView to restore the
+              // scroll position when a user leaves and returns to the app after it
+              // has been killed while running in the background.
+              restorationId: 'tasksListView',
+              itemCount: snapshot.hasData ? snapshot.data!.length : 0,
+              itemBuilder: (BuildContext context, int index) {
+                final item = snapshot.data![index];
 
-            return TaskListTile(task: item);
+                return TaskListTile(task: item);
+              },
+            );
           },
         ),
       ),
