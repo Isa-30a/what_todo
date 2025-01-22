d93c73185bfddb01a2fd95b00c2e707868323ac8
Isa-30a <isabella.alvasol@gmail.com>
2025-01-22 12:56:18 -0500

    🐛 fix: refactor TaskListTile by extracting CheckButton for improved readability and maintainability

--- lib/ui/add_task/widgets/task_list_tile.dart
+++ lib/ui/add_task/widgets/task_list_tile.dart
@@ -33,39 +33,7 @@ class TaskListTile extends StatelessWidget {
           );
         },
       ),
-      leading: Consumer<TodoProvider>(
-        builder: (context, value, child) {
-          return TextButton(
-            onPressed: () {
-              Provider.of<TodoProvider>(context, listen: false).updateTask(
-                task.copyWith(completed: !task.completed),
-              );
-            },
-            child: Container(
-              padding: EdgeInsets.all(2),
-              decoration: BoxDecoration(
-                gradient: LinearGradient(
-                  colors: [Colors.blue, Colors.purple],
-                  begin: Alignment.topLeft,
-                  end: Alignment.bottomRight,
-                ),
-                borderRadius: BorderRadius.all(Radius.circular(100)),
-              ),
-              height: 30,
-              width: 30,
-              child: (task.completed)
-                  ? SvgPicture.asset(
-                      Assets.iconcheck,
-                      fit: BoxFit.scaleDown,
-                    )
-                  : CircleAvatar(
-                      radius: 1,
-                      backgroundColor: Theme.of(context).canvasColor,
-                    ),
-            ),
-          );
-        },
-      ),
+      leading: CheckButton(task: task.completed),
       trailing: IconButton(
         onPressed: () {
           if (task.id != null) {
@@ -75,6 +43,52 @@ class TaskListTile extends StatelessWidget {
         },
         icon: Icon(Icons.close),
       ),
+    );
+  }
+}
+
+class CheckButton extends StatelessWidget {
+  const CheckButton({
+    super.key,
+    required this.task,
+  });
+
+  final Task task;
+
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<TodoProvider>(
+      builder: (context, value, child) {
+        return TextButton(
+          onPressed: () {
+            Provider.of<TodoProvider>(context, listen: false).updateTask(
+              task.copyWith(completed: !task.completed),
+            );
+          },
+          child: Container(
+            padding: EdgeInsets.all(2),
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                colors: [Colors.blue, Colors.purple],
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+              ),
+              borderRadius: BorderRadius.all(Radius.circular(100)),
+            ),
+            height: 30,
+            width: 30,
+            child: (task.completed)
+                ? SvgPicture.asset(
+                    Assets.iconcheck,
+                    fit: BoxFit.scaleDown,
+                  )
+                : CircleAvatar(
+                    radius: 1,
+                    backgroundColor: Theme.of(context).canvasColor,
+                  ),
+          ),
+        );
+      },
     );
   }
 }
