3f34d9c3631a525cfa7b7d49acf556294cf01ae7
Isa-30a <isabella.alvasol@gmail.com>
2025-01-27 15:46:24 -0500

    💄 ui: enhance TasksListView layout by adding item count display and clear completed button

--- lib/ui/add_task/widgets/tasks_list_view.dart
+++ lib/ui/add_task/widgets/tasks_list_view.dart
@@ -5,18 +5,11 @@ import 'package:what_todo/ui/add_task/widgets/task_list_tile.dart';
 
 /// Displays a list of SampleItems.
 class TasksListView extends StatelessWidget {
-  const TasksListView({super.key});
-
+  const TasksListView({super.key, this.child});
+  final Widget? child;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // To work with lists that may contain a large number of items, it’s best
-      // to use the ListView.builder constructor.
-      //
-      // In contrast to the default ListView constructor, which requires
-      // building all Widgets up front, the ListView.builder constructor lazily
-      // builds Widgets as they’re scrolled into view.
-
       body: Consumer<TodoProvider>(
         builder: (context, notifier, child) {
           return RefreshIndicator(
@@ -29,26 +22,47 @@ class TasksListView extends StatelessWidget {
               child: FutureBuilder(
                 future: notifier.futureData,
                 builder: (context, snapshot) {
-                  return ListView.separated(
-                    separatorBuilder: (BuildContext context, int index) =>
-                        Divider(color: Theme.of(context).disabledColor),
-                    // Providing a restorationId allows the ListView to restore the
-                    // scroll position when a user leaves and returns to the app after it
-                    // has been killed while running in the background.
-                    restorationId: 'tasksListView',
-                    itemCount: snapshot.hasData ? snapshot.data!.length : 0,
-                    itemBuilder: (BuildContext context, int index) {
-                      if (snapshot.connectionState == ConnectionState.waiting) {
-                        return Center(child: CircularProgressIndicator());
-                      } else if (snapshot.hasError) {
-                        return Text('Error: ${snapshot.error}');
-                      } else if (snapshot.hasData) {
-                        final item = snapshot.data![index];
-                        return TaskListTile(task: item);
-                      } else {
-                        return Text('Sin datos');
-                      }
-                    },
+                  return Column(
+                    children: [
+                      Expanded(
+                        child: ListView.separated(
+                          separatorBuilder: (BuildContext context, int index) =>
+                              Divider(color: Theme.of(context).disabledColor),
+                          restorationId: 'tasksListView',
+                          itemCount:
+                              snapshot.hasData ? snapshot.data!.length : 0,
+                          itemBuilder: (BuildContext context, int index) {
+                            if (snapshot.connectionState ==
+                                ConnectionState.waiting) {
+                              return Center(child: CircularProgressIndicator());
+                            } else if (snapshot.hasError) {
+                              return Text('Error: ${snapshot.error}');
+                            } else if (snapshot.hasData) {
+                              final item = snapshot.data![index];
+                              return TaskListTile(task: item);
+                            } else {
+                              return Text('Sin datos');
+                            }
+                          },
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          children: [
+                            Text(snapshot.hasData
+                                ? '${snapshot.data!.length} items left'
+                                : 'No items left'),
+                            this.child ?? SizedBox(),
+                            TextButton(
+                              onPressed: () {},
+                              child: Text('Clear completed'),
+                            )
+                          ],
+                        ),
+                      ),
+                    ],
                   );
                 },
               ),
