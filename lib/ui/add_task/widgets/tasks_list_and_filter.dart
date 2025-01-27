bdbde2a99b5b768b49940b4b8a7ea170f2b2dc76
Isa-30a <isabella.alvasol@gmail.com>
2025-01-27 15:46:43 -0500

    💄 ui: add TasksListAndFilter widget for improved task filtering and layout

---
+++ lib/ui/add_task/widgets/tasks_list_and_filter.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:what_todo/ui/add_task/widgets/tasks_list_view.dart';
+
+class TasksListAndFilter extends StatelessWidget {
+  const TasksListAndFilter({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    Widget filterTab = Card(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          TextButton(onPressed: () {}, child: Text('All')),
+          TextButton(onPressed: () {}, child: Text('Active')),
+          TextButton(onPressed: () {}, child: Text('Completed'))
+        ],
+      ),
+    );
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        double maxWidth = constraints.maxWidth;
+
+        return Column(
+          children: [
+            SizedBox(
+              height: MediaQuery.sizeOf(context).height / 2,
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(16),
+                child: TasksListView(child: maxWidth >= 600 ? filterTab : null),
+              ),
+            ),
+            if (maxWidth < 600) filterTab,
+          ],
+        );
+      },
+    );
+  }
+}
