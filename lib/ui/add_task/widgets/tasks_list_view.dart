ce24466a39b29a8f9f96509d91c96f771b458a80
Isa-30a <isabella.alvasol@gmail.com>
2025-01-29 22:24:46 -0500

    💄 ui: implement reorderable list for tasks in TasksListView

--- lib/ui/add_task/widgets/tasks_list_view.dart
+++ lib/ui/add_task/widgets/tasks_list_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:what_todo/data/model/task.dart';
 import 'package:what_todo/ui/add_task/view_model/todo_provider.dart';
 import 'package:what_todo/ui/add_task/widgets/task_list_tile.dart';
 
@@ -32,12 +33,10 @@ class _TasksListViewState extends State<TasksListView> {
                   return Column(
                     children: [
                       Expanded(
-                        child: ListView.separated(
-                          separatorBuilder: (BuildContext context, int index) =>
-                              Divider(color: Theme.of(context).disabledColor),
-                          restorationId: 'tasksListView',
-                          itemCount:
-                              snapshot.hasData ? snapshot.data!.length : 0,
+                        child: ReorderableListView.builder(
+                          // separatorBuilder: (BuildContext context, int index) =>
+                          //     Divider(color: Theme.of(context).disabledColor),
+                          buildDefaultDragHandles: false,
                           itemBuilder: (BuildContext context, int index) {
                             if (snapshot.connectionState ==
                                 ConnectionState.waiting) {
@@ -46,10 +45,42 @@ class _TasksListViewState extends State<TasksListView> {
                               return Text('Error: ${snapshot.error}');
                             } else if (snapshot.hasData) {
                               final item = snapshot.data![index];
-                              return TaskListTile(task: item);
+                              return ReorderableDragStartListener(
+                                key: ValueKey(item.id),
+                                index: index,
+                                child: TaskListTile(task: item),
+                              );
                             } else {
                               return Text('Sin datos');
                             }
+                          },
+                          restorationId: 'tasksListView',
+                          itemCount:
+                              snapshot.hasData ? snapshot.data!.length : 0,
+                          // itemBuilder: (BuildContext context, int index) {
+                          //   if (snapshot.connectionState ==
+                          //       ConnectionState.waiting) {
+                          //     return Center(child: CircularProgressIndicator());
+                          //   } else if (snapshot.hasError) {
+                          //     return Text('Error: ${snapshot.error}');
+                          //   } else if (snapshot.hasData) {
+                          //     final item = snapshot.data![index];
+                          //     return TaskListTile(key: ValueKey(index), task: item);
+                          //   } else {
+                          //     return Text('Sin datos');
+                          //   }
+                          // },
+                          onReorder: (int oldIndex, int newIndex) {
+                            setState(() {
+                              if (oldIndex < newIndex) {
+                                newIndex -= 1;
+                              }
+                              if (snapshot.hasData) {
+                                final Task item =
+                                    snapshot.data!.removeAt(oldIndex);
+                                snapshot.data!.insert(newIndex, item);
+                              }
+                            });
                           },
                         ),
                       ),
